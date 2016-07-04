9aa78c5bbcd94ac56af8de85f6653294f1c14be1
shangzhenwei <563410019@qq.com>
2016-07-04 10:37:03 +0800

    commit master2_2.

--- git.test/src/main/java/com/testsample/git/web/java/TestMaster1_2.java
+++ git.test/src/main/java/com/testsample/git/web/java/TestMaster1_2.java
@@ -5,6 +5,7 @@ public class TestMaster1_2 {
 	public static void main(String[] args) {
 		System.out.println("master1_2");
 		System.out.println("master2_1");
+		System.out.println("master2_1");
 	}
 
 }
--- git.test/src/main/java/com/testsample/git/web/java/TestMaster2_1.java
+++ git.test/src/main/java/com/testsample/git/web/java/TestMaster2_1.java
@@ -4,6 +4,7 @@ public class TestMaster2_1 {
 
 	public static void main(String[] args) {
 		System.out.println("master2_1");
+		System.out.println("master2_2");
 	}
 
 }
