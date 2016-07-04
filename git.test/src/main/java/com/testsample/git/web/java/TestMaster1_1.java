9318cbca09131a447ae957729047f47d47327912
shangzhenwei <563410019@qq.com>
2016-07-04 10:19:40 +0800

    master1.

---
+++ git.test/src/main/java/com/testsample/git/web/java/TestMaster1_1.java
@@ -0,0 +1,9 @@
+package com.testsample.git.web.java;
+
+public class TestMaster1_1 {
+
+	public static void main(String[] args) {
+		System.out.println("master1_1");
+	}
+
+}
---
+++ git.test/src/main/java/com/testsample/git/web/java/TestMaster1_2.java
@@ -0,0 +1,9 @@
+package com.testsample.git.web.java;
+
+public class TestMaster1_2 {
+
+	public static void main(String[] args) {
+		System.out.println("master1_2");
+	}
+
+}
