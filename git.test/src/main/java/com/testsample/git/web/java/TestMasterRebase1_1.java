95d2636093e4c31fad81078cbb40ed94314549eb
shangzhenwei <563410019@qq.com>
2016-07-04 14:59:36 +0800

    master rebase1

---
+++ git.test/src/main/java/com/testsample/git/web/java/TestMasterRebase1_1.java
@@ -0,0 +1,9 @@
+package com.testsample.git.web.java;
+
+public class TestMasterRebase1_1 {
+
+	public static void main(String[] args) {
+		System.out.println("master rebase1_1");
+	}
+
+}
--- git.test/src/main/java/com/testsample/git/web/java/TestReset1.java
+++ git.test/src/main/java/com/testsample/git/web/java/TestReset1.java
@@ -4,6 +4,7 @@ public class TestReset1 {
 
 	public static void main(String[] args) {
 		System.out.println("reset1");
+		System.out.println("master rebase1_1");
 		System.out.println("branch rebase 1_1");
 		System.out.println("branch rebase 2_1");
 	}
