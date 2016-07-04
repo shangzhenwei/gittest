cbc0793a5628a29092c148bc17701538b0b76c1a
shangzhenwei <563410019@qq.com>
2016-07-04 10:11:04 +0800

    delete old files.

--- git.test/src/main/java/com/testsample/git/web/java/TestGit.java
+++
@@ -1,9 +0,0 @@
-package com.testsample.git.web.java;
-
-public class TestGit {
-
-	public static void main(String[] args) {
-		System.out.println("Test Git."); 
-	}
-
-}
--- git.test/src/main/java/com/testsample/git/web/java/TestGit5.java
+++
@@ -1,9 +0,0 @@
-package com.testsample.git.web.java;
-
-public class TestGit5 {
-
-	public static void main(String[] args) {
-		System.out.println("test git branch merger.");
-	}
-
-}
