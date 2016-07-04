1b58ce5315a45d314ae999e5f5c5c451025338ef
shangzhenwei <563410019@qq.com>
2016-07-04 14:55:48 +0800

    branch rebase1

--- git.test/src/main/java/com/testsample/git/web/java/TestBranchRebase1_1.java
+++ git.test/src/main/java/com/testsample/git/web/java/TestBranchRebase1_1.java
@@ -1,9 +1,8 @@
 package com.testsample.git.web.java;
 
-public class TestReset1 {
+public class TestBranchRebase1_1 {
 
 	public static void main(String[] args) {
-		System.out.println("reset1");
 		System.out.println("branch rebase 1_1");
 	}
 
