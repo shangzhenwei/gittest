46c208a242e6c90a1eca15ad4fabdac8b1aa2fa6
shangzhenwei <563410019@qq.com>
2016-07-04 14:57:39 +0800

    branch rebase2

--- git.test/src/main/java/com/testsample/git/web/java/TestBranchRebase2_1.java
+++ git.test/src/main/java/com/testsample/git/web/java/TestBranchRebase2_1.java
@@ -1,10 +1,8 @@
 package com.testsample.git.web.java;
 
-public class TestReset1 {
+public class TestBranchRebase2_1 {
 
 	public static void main(String[] args) {
-		System.out.println("reset1");
-		System.out.println("branch rebase 1_1");
 		System.out.println("branch rebase 2_1");
 	}
 
