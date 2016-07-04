dd04d76624aa6f6e7a36620340241ce1848f273b
shangzhenwei <563410019@qq.com>
2016-07-04 10:35:30 +0800

    commit master2_1.

--- git.test/src/main/java/com/testsample/git/web/java/TestMaster2_1.java
+++ git.test/src/main/java/com/testsample/git/web/java/TestMaster2_1.java
@@ -1,9 +1,8 @@
 package com.testsample.git.web.java;
 
-public class TestMaster1_2 {
+public class TestMaster2_1 {
 
 	public static void main(String[] args) {
-		System.out.println("master1_2");
 		System.out.println("master2_1");
 	}
 
